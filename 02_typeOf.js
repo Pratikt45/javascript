2e486c936d20b7e85b93998cb19ce9351a1fff4f
Pratik Thorat <pratikthorat3727@gmail.com>
2023-05-08 13:48:50 +0530

    JS
    JavaScript basics, primirive, referenced type

---
+++ 02_typeOf.js
@@ -0,0 +1,38 @@
+
+console.log("Before Initialization");
+var bankName;
+console.log("Bank Name : " +  typeof dataType);
+var dataType;
+console.log("Data Type : " +  typeof dataType);
+
+console.log("After Initialization");
+var bankName = "SBI Bank";
+console.log("Bank Name : " + bankName);
+var dataType = " ";
+console.log("Data Type : " +  typeof dataType);
+console.log("----------------------------------");
+
+var mark = '90'
+console.log("Marks : "+ mark);
+var dataType = " ";
+console.log("Data Type : " +  typeof dataType);
+console.log("----------------------------------");
+
+console.log("Before Initialization");
+var value ;
+console.log("Value : " +  typeof value);
+console.log("After Initialization");
+var value = true ;
+console.log("Value : " + value);
+var isWorking = typeof value;
+console.log("Type of isWorking : "+isWorking);
+console.log("----------------------------------");
+
+var totalCount = "Hundered and Seven";
+console.log("value : " +totalCount);
+var isWorking = typeof totalCount;
+console.log("Type of isWorking : "+isWorking);
+var totalCount = 107;
+console.log("value : " +totalCount);
+var isWorking = typeof totalCount;
+console.log("Type of isWorking : "+isWorking);
---
+++ hello.js
@@ -0,0 +1,21 @@
+
+
+
+
+console.log("Pratik Thorat");
+console.log("Arvi, Dist. Wardha Pin:-442201");
+
+var firstName = "Pratik";
+console.log(firstName);
+
+var age;    //variable declaration
+age = "22"; //variable initialization
+console.log(age);
+
+var holder;    //variable declaration
+console.log(holder); //undefined variable
+holder = "My Spec"; //variable initialization
+console.log(holder);
+
+holder = "Sachin Spec"; //variable update
+console.log(holder);    //
