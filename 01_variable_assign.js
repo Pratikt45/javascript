48ae36931e5e30a27a4d9b899e9912b4b39d1859
Pratik Thorat <pratikthorat3727@gmail.com>
2023-05-08 13:50:18 +0530

    Assignments
    First and Second

---
+++ 01_variable_assign.js
@@ -0,0 +1,31 @@
+//JS  Assignment No -- 1
+
+//PRoblem no 1
+var myFullName;
+console.log(myFullName);
+myFullName = "Pratik Thorat";
+console.log(myFullName);
+myFullName = "Pratik Ajayrao Thorat";
+console.log(myFullName);
+
+//Problem  no 2
+var myLove;
+myLove = "I Love Only JS";
+console.log(myLove);
+
+//Problem no 3
+var firstName;
+firstName = "Pratik";
+var lastName;
+lastName = "Thorat";
+var age;
+age = "22";
+var address;
+address = "Arvi, Dist. Wardha";
+var pincode;
+pincode = "442201";
+var gender;
+gender = "Male";
+var isMarried;
+isMarried = "No";
+console.log(firstName, lastName, age, address,  pincode,  gender, isMarried);
---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,22 @@
+// JS  Assignment No --  2
+
+var pinCode = 442201;
+console.log("Arvi:" ,pinCode,' ',"type:", typeof pinCode);
+var firstName = "Pratik"; 
+console.log("Name:" ,firstName,' ',"type:", typeof firstName);
+var isSoftwareEngineer = true; 
+console.log("isSoftwareEngineer:" ,isSoftwareEngineer,' ',"type:", typeof isSoftwareEngineer);
+
+console.log("                                 ");
+console.log("                                 ");
+console.log("                                 ");
+console.log("* Difference Between Null and Undefined");
+console.log("                                 ");
+console.log('---------------------------------------------------------------------');
+console.log('        Null         ','          |','        Undefined         ');
+console.log('---------------------------------------------------------------------');
+var name = null;
+var laptop;
+console.log('     value: ',name,     "              |       ",  'laptop: ',laptop);
+console.log('     type of value: ',typeof laptop,     " |       ",  'type of laptop: ',typeof laptop);
+console.log('---------------------------------------------------------------------');
